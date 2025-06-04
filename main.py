3c904a6c673ff122d2e54f7bfedfec411645d9c6
Andrew Petrulakis <petrulakisandrew@gmail.com>
2025-06-04 15:22:32 -0500

    Importing DataFrames

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 #Imports
 import pandas as pd
+
+#Importing Tenant Info and FMR DataFrames
+tenant_df = pd.read_excel('C:/Users/Andrew Petrulakis/Desktop/Reports/Rent Analysis/Rent Burden - FMR Analysis (DHA HCV)/Tenant Info.xlsx')
+# print(tenant_df)
+
+fmr_df = pd.read_excel('C:/Users/Andrew Petrulakis/Desktop/Reports/Rent Analysis/Rent Burden - FMR Analysis (DHA HCV)/DuPage Small Area FMRs.xlsx')
+# print(fmr_df)
